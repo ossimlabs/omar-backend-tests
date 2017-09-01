fd66b9294b1701e1969080b9a5a3a360ed749d09
Stephen L'Allier <stephen.lallier@digitalglobe.com>
2017-09-01 09:32:52 -0400

    update default conf

--- src/cucumber/resources/cucumber-config.groovy
+++ src/cucumber/resources/cucumber-config.groovy
@@ -1,16 +1,9 @@
 rbtcloudRootDir = "https://omar-dev.ossim.io"
-sqsStagingQueue = "https://sqs.us-east-1.amazonaws.com/320588532383/avro-dev"
 s3Bucket = "o2-test-data/Standard_test_imagery_set"
 s3BucketUrl = "https://s3.amazonaws.com"
-s3WmsVerificationFiles = "WMS_verification_images"
 s3WcsVerificationFiles = "WCS_verification_images"
 s3BasemapVerificationFiles = "Basemap_verification_images"
 
-downloadService = "${rbtcloudRootDir}/omar-download"
-icpServerProperty = "${rbtcloudRootDir}/omar-wms/imageSpace"
-//ingestBucket = "o2-test-data/Ingest_Staging_Folder"
-stagingService = "${rbtcloudRootDir}/omar-stager/dataManager"
-tlvUrl = "${rbtcloudRootDir}/tlv"
 wfsServerProperty = "${rbtcloudRootDir}/omar-wfs/wfs"
 wmsServerProperty = "${rbtcloudRootDir}/omar-wms/wms"
 wcsServerProperty = "${rbtcloudRootDir}/omar-wcs/wcs"
@@ -25,6 +18,7 @@ wmtsUrl = "${rbtcloudRootDir}/omar-wmts"
 
 // minutes to wait
 waitForStage = 5
+
 images = [
         geoeye: [
                 msi: [
