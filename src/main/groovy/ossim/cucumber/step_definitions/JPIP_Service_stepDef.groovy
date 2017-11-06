b94c059ab0543531ec8b95746d2330cc0d415770
anies <arocnies@gmail.com>
2017-11-06 16:18:09 -0500

    Removed quotes around file path Fixed assertion in JPIP test

--- src/main/groovy/ossim/cucumber/step_definitions/JPIP_Service_stepDef.groovy
+++ src/main/groovy/ossim/cucumber/step_definitions/JPIP_Service_stepDef.groovy
@@ -29,7 +29,8 @@ When(~/^a call is made to JPIP to create a stream of an image at (.*) entry (.*)
         // Need to wait until the image has been processed.
         // The initial "READY" state turns to "FINISHED" when done.
         assert waitForTrueOrTimeout(timeoutInMillis) {
-            jpipResponse = jsonSlurper.parse(createStreamUrl)["status"] == "FINISHED"
+            jpipResponse = jsonSlurper.parse(createStreamUrl)["status"]
+            jpipResponse == "FINISHED"
         }
 }
 
