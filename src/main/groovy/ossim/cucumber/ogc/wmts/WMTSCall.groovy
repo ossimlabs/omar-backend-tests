2c9692be35d05ff4148f84f3fd72a3c2c2df1643
Garrett Potts <gpotts@radiantblue.com>
2017-11-09 08:12:07 -0500

    Added styles so it is passed to WMS

--- src/main/groovy/ossim/cucumber/ogc/wmts/WMTSCall.groovy
+++ src/main/groovy/ossim/cucumber/ogc/wmts/WMTSCall.groovy
@@ -67,6 +67,7 @@ class WMTSCall
         params.tileCol = centerTileInfo.tile.x
         params.tileMatrix = centerTileInfo.tile.z
         params.tileMatrixSet = layer.tileMatrixName
+        params.styles = URLEncoder.encode("""{"nullPixelFlip": false}""", Charset.defaultCharset().displayName()),
 
         String urlParamsString = urlParamsToString(params)
         String url = "${wmtsServer}/getTile?${urlParamsString}"
@@ -94,6 +95,7 @@ class WMTSCall
         params.tileCol = 0
         params.tileMatrix = layer.maxLevel
         params.tileMatrixSet = layer.tileMatrixName
+        params.styles = URLEncoder.encode("""{"nullPixelFlip": false}""", Charset.defaultCharset().displayName()),
 
         String urlParamsString = urlParamsToString(params)
         String url = "${wmtsServer}/getTile?${urlParamsString}"
