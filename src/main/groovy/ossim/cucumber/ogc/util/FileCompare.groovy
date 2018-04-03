e54d74de051ea4b6f052e4ad489fc21ce7111114
arocnies <arocnies@gmail.com>
2018-04-03 13:38:40 -0400

    Added image comparison to the compareFiles.

--- src/main/groovy/ossim/cucumber/ogc/util/FileCompare.groovy
+++ src/main/groovy/ossim/cucumber/ogc/util/FileCompare.groovy
@@ -1,6 +1,11 @@
 package ossim.cucumber.ogc.util
 
+import com.sun.istack.internal.NotNull
 import org.apache.commons.io.FileUtils
+
+import javax.imageio.ImageIO
+import java.awt.image.BufferedImage
+import java.awt.image.DataBuffer
 
 class FileCompare
 {
@@ -26,6 +31,41 @@ class FileCompare
 
     static boolean checkImages(File file1, File file2)
     {
-        FileUtils.contentEquals(file1, file2)
+        double matchPercent = compareImage(file1, file2)
+        println "Image match percent: $matchPercent"
+        return (matchPercent > 90.0)
+    }
+
+    private static double compareImage(@NotNull File fileA, @NotNull File fileB) {
+        if (!fileA.exists() || !fileB.exists()) return 100
+        double percentage = 0
+        try {
+            // Take buffer data from both image files.
+            BufferedImage biA = ImageIO.read(fileA)
+            DataBuffer dbA = biA.getData().getDataBuffer()
+            int sizeA = dbA.getSize()
+            BufferedImage biB = ImageIO.read(fileB)
+            DataBuffer dbB = biB.getData().getDataBuffer()
+            int sizeB = dbB.getSize()
+
+            // Compare data-buffer objects.
+            int count = 0
+            if (sizeA == sizeB) {
+                for (int i = 0; i < sizeA; i++) {
+                    if (dbA.getElem(i) == dbB.getElem(i)) {
+                        count = count + 1
+                    }
+                }
+                percentage = (count * 100) / sizeA
+            } else {
+                System.out.println("Both the images are not of same size")
+                percentage == 100
+            }
+
+        } catch (Exception e) {
+            System.out.println("Failed to compare image files ...")
+            throw e
+        }
+        return percentage
     }
 }
