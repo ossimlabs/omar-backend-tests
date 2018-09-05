e41b527993f0eb0583abb761392ebb40d719e747
oscarkramer <oscar.kramer@radiantsolutions.com>
2018-09-05 16:35:00 -0400

    Consolidated checkImages so that all tests use cross-correlation

--- src/main/groovy/ossim/cucumber/ogc/util/FileCompare.groovy
+++ src/main/groovy/ossim/cucumber/ogc/util/FileCompare.groovy
@@ -15,8 +15,8 @@ class FileCompare
     static boolean checkImages(URL filePath1, URL filePath2, String image_type=null)
     {
         println "\nFileCompare::checkImages(URL,URL)"
-        println "    ${filePath1}"
-        println "    ${filePath2}"
+        println ">>>    ${filePath1}"
+        println ">>>    ${filePath2}"
         String suffix = image_type ? ".${image_type}" : ""
         File file1 = File.createTempFile("tempImage1", suffix)
         File file2 = File.createTempFile("tempImage2", suffix)
@@ -24,8 +24,7 @@ class FileCompare
         FileUtils.copyURLToFile(filePath1, file1)
         FileUtils.copyURLToFile(filePath2, file2)
 
-        boolean imagesEqual = FileUtils.contentEquals(file1, file2)
-        println "Exact match: ${imagesEqual}"
+        boolean imagesEqual = checkImages(file1, file2)
 
         file1.deleteOnExit()
         file2.deleteOnExit()
@@ -35,58 +34,47 @@ class FileCompare
 
     static boolean checkImages(File file1, File file2)
     {
-        println "\nFileCompare::checkImages(${file1}, ${file2})"
+      if (!fileA.exists() || !fileB.exists())
+          return 0
 
-        double correlation = compareImage(file1, file2)
-        println "Image Correlation: ${correlation}"
-        return (correlation > 0.99)
+      double correlation = 0
+      try
+      {
+         // Take buffer data from both image files.
+         BufferedImage biA = ImageIO.read(fileA)
+         DataBuffer dbA = biA.getData().getDataBuffer()
+         int sizeA = dbA.getSize()
+         BufferedImage biB = ImageIO.read(fileB)
+         DataBuffer dbB = biB.getData().getDataBuffer()
+         int sizeB = dbB.getSize()
+         if (sizeA != sizeB)
+         {
+            System.out.println("Images are not of same size")
+            return 0
+         }
 
-        //double matchPercent = compareImage(file1, file2)
-        //println "Image match percent: ${matchPercent}\n"
-        //return (matchPercent > 90.0)
-    }
+         // Compare data-buffer objects by computing normailized cross-correlation:
+         double sumA2=0
+         double sumB2=0
+         double sumAB=0
+         double a, b
+         for (int i = 0; i < sizeA; i++)
+         {
+            a = dbA.getElem(i);
+            b = dbB.getElem(i);
+            sumA2 += a * a;
+            sumB2 += b * b;
+            sumAB += a * b;
+         }
+         correlation = sumAB / sqrt(sumA2 * sumB2);
+      }
+      catch (Exception e)
+      {
+         System.out.println("Failed to compare image files ...")
+         throw e
+      }
 
-    private static double compareImage(@NotNull File fileA, @NotNull File fileB) {
-        if (!fileA.exists() || !fileB.exists()) return 100
-        //double percentage = 0
-        double correlation = 0
-        try {
-            // Take buffer data from both image files.
-            BufferedImage biA = ImageIO.read(fileA)
-            DataBuffer dbA = biA.getData().getDataBuffer()
-            int sizeA = dbA.getSize()
-            BufferedImage biB = ImageIO.read(fileB)
-            DataBuffer dbB = biB.getData().getDataBuffer()
-            int sizeB = dbB.getSize()
-
-            // Compare data-buffer objects.
-            //int count = 0
-            double sumA2=0
-            double sumB2=0
-            double sumAB=0
-            double a, b
-            if (sizeA == sizeB) {
-                for (int i = 0; i < sizeA; i++) {
-                   a = dbA.getElem(i);
-                   b = dbB.getElem(i);
-                   sumA2 += a * a;
-                   sumB2 += b * b;
-                   sumAB += a * b;
-                  //  if (dbA.getElem(i) == dbB.getElem(i)) {
-                  //      count = count + 1
-                  //  }
-                }
-                correlation = sumAB / sqrt(sumA2 * sumB2);
-                //percentage = (count * 100) / sizeA
-            } else {
-                System.out.println("Images are not of same size")
-            }
-
-        } catch (Exception e) {
-            System.out.println("Failed to compare image files ...")
-            throw e
-        }
-        return correlation
-        //return percentage
+      println "FileCompare::checkImages()  Image Correlation: ${correlation}"
+      return (correlation > 0.99)
     }
 }
